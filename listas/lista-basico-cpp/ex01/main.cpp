630b6b1885315d13e6276efa58ba778bb26e4963
Michelk55 <123950145+Michelk55@users.noreply.github.com>
2026-02-11 20:15:56 -0300

    Exercício 1: Olá Mundo Personalizado

--- listas/lista-basico-cpp/ex01/main.cpp
+++ listas/lista-basico-cpp/ex01/main.cpp
@@ -1,15 +1,3 @@
-/**
- * @exercise Lista Básico C++ - Exercício 1
- * @title Olá Mundo Personalizado
- * @description Modifique o programa "Olá, mundo!" para pedir e ler o nome do usuário e exibir uma saudação personalizada.
- * @input stdin
- * @output stdout
- * @timeout 1000
- * @test name="Nome simples" input="João" expected="Olá, João!"
- * @test name="Nome composto" input="Maria Silva" expected="Olá, Maria Silva!"
- * @test name="Nome vazio" input="" expected="Olá, !"
- */
-
 #include <iostream>
 #include <string>
 
@@ -17,10 +5,11 @@ using namespace std;
 
 int main() {
     string nome;
-    
-    // TODO: Leia o nome do usuário
-    
-    // TODO: Exiba a saudação personalizada
-    
+
+    getline(cin, nome);
+
+    cout << "Olá, " << nome << "!" << endl;
+
     return 0;
 }
+
